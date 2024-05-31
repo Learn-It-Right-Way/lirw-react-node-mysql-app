4e9a74263517154c250b13d91fa6fd269ffa573a
Dilan Madhusanka <38487189+kadm0128@users.noreply.github.com>
2024-05-31 16:13:08 +0800

    [LIRW] - AWS SDK secrets manager changes added

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,16 +1,42 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "12345678",
-    database: "react_node_app",
-    synchronize: true,
-    logging: false,
-    entities: ["src/entity/*.ts"],
-    migrations: [],
-    subscribers: [],
-})
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-east-1' });
+
+async function getSecretValue(secretName: string) {
+    const client = new AWS.SecretsManager();
+    try {
+        const data = await client.getSecretValue({ SecretId: secretName }).promise();
+        return JSON.parse(data.SecretString);
+    } catch (err) {
+        console.error('Error retrieving secret:', err);
+        throw err;
+    }
+};
+
+async function connectToDatabase() {
+    const secretName = 'learn-it-right-way';
+    const credentials = await getSecretValue(secretName);
+
+    const appDataSource = new DataSource({
+        type: "mysql",
+        host: credentials.host,
+        port: 3306,
+        username: credentials.username,
+        password: credentials.password,
+        database: credentials.database,
+        synchronize: true,
+        logging: false,
+        entities: ["src/entity/*.ts"],
+        migrations: [],
+        subscribers: [],
+    });
+
+    return appDataSource;
+}
+
+
+
+export const AppDataSource = connectToDatabase();
