eebcaade1af2ca09c97fd5b3db5607f12afbe8ef
Learn It Right Way <learnitrightway01@gmail.com>
2025-03-10 23:01:12 +0800

    [LIRW] - Containerized frontend and backend using Docker

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const host = process.env.DB_HOST || 'localhost';
-const port = process.env.DB_PORT || '3306';
-const user = process.env.DB_USER || 'root';
-const password = process.env.DB_PASSWORD || '12345678';
-const database = process.env.DB_NAME || 'react_node_app';
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
 
 const db = mysql.createConnection({
    host: host,
---
+++ backend/ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+   apps : [{
+     name   : "backend",
+     script : "./server.js"
+   }]
+ }
